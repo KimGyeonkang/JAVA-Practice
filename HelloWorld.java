7bbcfc81b87a5474b6a81173873cd4db2bcc2652
KimGyeonkang <v.tang3203@gmail.com>
2026-01-02 21:43:12 +0900

    First Day
    자바 기본 연습(<Do it! 자바 프로그래밍 입문 [개정판]> 스터디)

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+2025년 12월 30일(화)
+첫 자바 프로그래밍 연습
+김견강
+*/
+
+package hello; //패키지명
+
+public class HelloWorld {  //클래스명
+    public static void main(String[] args){
+        System.out.println("Hello, World!"); //System.out.println("~") 자바에서 출력 명령문
+    }
+}
---
+++ SayMyName.java
@@ -0,0 +1,7 @@
+package hello; //패키지명
+
+public class SayMyName { //클래스명, 새 클래스 생성
+    public static void main(String[] args){
+        System.out.println("이름:" + " " + "김견강"); //출력 명령 응용 연습
+    }
+}
