3cb8b456bbc55eb4d48b59c5942517ac9157233a
KimGyeonkang <v.tang3203@gmail.com>
2026-01-03 21:13:37 +0900

    Variable1 Second commit
    change a variable: level -> age
    Printing my age lol

--- Variable1.java
+++ Variable1.java
@@ -2,8 +2,8 @@ package chapter2;
 
 public class Variable1 {
     public static void main(String[] args){
-        int level; //변수 선언
-        level = 10; //변수에 값 대입
-        System.out.println(level);
+        int age; //변수 선언
+        age = 27; //변수에 값 대입
+        System.out.println(age);
     }
 }
