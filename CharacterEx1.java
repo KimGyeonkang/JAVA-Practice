8a3e9dc7b8a842b004991a4cd3334fc238785b35
KimGyeonkang <v.tang3203@gmail.com>
2026-01-03 21:20:30 +0900

    Third commit
    rewrite some comments

--- CharacterEx1.java
+++ CharacterEx1.java
@@ -6,11 +6,11 @@ public class CharacterEx1 {
         System.out.println(ch1); //문자 그대로 출력
         System.out.println((int)ch1); //문자를 아스키 코드로 변환 출력
 
-        char ch2 = 38; //정수의 값 대입, B의 아스키 코드는 66
+        char ch2 = 38; //정수의 값 대입, &의 아스키 코드는 38
         System.out.println(ch2); //정숫값에 해당하는 문자 출력
 
         int ch3 = 97; // 정수 자료형 변수 선언
         System.out.println(ch3); // 정숫값 그대로 출력
-        System.out.println((char)ch3); // 정수를 문자 자료형으로 변환 출력. 아스키 코드가 67인 문자는 C
+        System.out.println((char)ch3); // 정수를 문자 자료형으로 변환 출력. 아스키 코드가 97인 문자는 a
     }
 }
